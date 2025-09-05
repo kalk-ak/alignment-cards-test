b3c459efd12248af7a79d42a99db71f955c58fec
Kaleb Aklilu <kalebaklilu3@gmail.com>
2025-09-04 21:31:05 -0400

    Update prototype0.js

--- prototype0.js
+++ prototype0.js
@@ -1,1 +1,10 @@
+export const cards = [
+  {
+    "name": "xxx",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },                                              //comma to separate objects in the array
 
+//reproduce the above structure for the other four
+];
