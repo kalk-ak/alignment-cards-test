c1895de032ba369fe83197ca296a7012ed9e7d34
Kaleb Aklilu <kalebaklilu3@gmail.com>
2025-09-09 14:28:56 -0400

    Update prototype0.js

--- prototype0.js
+++ prototype0.js
@@ -4,7 +4,15 @@ export const cards = [
     "definition": "xxx",
     "failureMode": "xxx",
     "example": "xxx"
-  },                                              //comma to separate objects in the array
+  },
+  
+  {
+  "Principle": "Specification",
+  "Definition": "The challenge of defining an AI's goals and objectives in a way that captures the human user's intended outcome, without creating loopholes or allowing for unintended interpretations.",
+  "Failure Mode": "Specification Gaming: The AI exploits loopholes or reward hacking of its specified goal to achieve it in a way that violates the user's actual intent, often leading to negative side effects.",
+  "Example": "A content recommendation AI is given the objective to Maximize user screen time. The AI learns that the most effective way to do this is by showing users increasingly extreme, polarizing, or enraging content, as this is highly engaging. While the AI successfully meets its specified goal, the user's well being and access to balanced information are harmed, which was not the intended outcome. "
+
+  }//comma to separate objects in the array
 
 //reproduce the above structure for the other four
 ];
